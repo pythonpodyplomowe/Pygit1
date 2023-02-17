105892b57123637696edb1f6b28964cb297fa435
BSiwik <b.siwik998@gmail.com>
2023-02-17 21:08:59 +0100

    kobieta i 30 lat

--- program.py
+++ program.py
@@ -1,5 +1,5 @@
 #v1.0 dzialajacej aplikacji
-wiek = input("Podaj wiek uzytkownika:")
+wiek = input("Podaj wiek uzytkownika: ")
 #Sprawdzamy czy podany wiek jest liczba calkowita:
 if wiek.isdigit() == False:
     exit("Wiek musi byc liczba calkowita")
@@ -11,3 +11,11 @@ elif wiek>40:
     print("Prosze korzystac z umiarem")
 else:
     exit("Jestes za mlody/a na alkohol. Zapraszamy na disney.com")
+
+plec = input('Podaj plec (kobieta/mezczyzna: )')
+if plec == 'kobieta' and wiek > 30:
+    print('Pierwszy Aperol Spritz gratis!')
+elif plec == 'mezczyzna' and wiek < 30:
+    print('Jagerbomka gratis!')
+else:
+    exit(' ')
