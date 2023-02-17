805580a294a79f12958ef2c557301dcff5386b03
History39 <kf83@protonmail.com>
2023-02-17 21:08:17 +0100

    age 120

--- program.py
+++ program.py
@@ -4,10 +4,14 @@ wiek = input("Podaj wiek uzytkownika:")
 if wiek.isdigit() == False:
     exit("Wiek musi byc liczba calkowita")
 wiek=int(wiek)
+if wiek>=120:
+    exit("wiek obsługiwanego musi być mniejszy niż 120 lat")
 if wiek>=18 and wiek <=40:
     print("Witaj w naszym sklepie z alkoholem")
-elif wiek>40:
+elif wiek > 40 and wiek <= 99:
     print("Witaj w naszym sklepie z alkoholem")
-    print("Prosze korzystac z umiarem")
+    print("Prosze korzystac z umiarem, dbaj o zdrowie")
+elif wiek>=100 and wiek <=119:
+    print("Ze względu na zaawansowany wiek nie powinieneś w ogóle pić alkoholu")
 else:
     exit("Jestes za mlody/a na alkohol. Zapraszamy na disney.com")
