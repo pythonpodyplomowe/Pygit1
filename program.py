e572cf5e3350680135cd37aa82c6f81cc5f10aa2
Nieznane też <alan.grabiasz1@email.com>
2023-02-17 21:08:44 +0100

    Region

--- program.py
+++ program.py
@@ -11,3 +11,14 @@ elif wiek>40:
     print("Prosze korzystac z umiarem")
 else:
     exit("Jestes za mlody/a na alkohol. Zapraszamy na disney.com")
+
+#Użytkownik wybiera region.
+inp = input("Wybierz swój region EUR albo USA ")
+
+if inp == "EUR":
+    print("Oferta dostępna dla regionu EUR")
+elif inp == "USA":
+    print("Oferta dostępna dla regionu USA")
+else:
+    print("Wybierz poprawny region EUR albo USA")
+
